7189b14ab281cc96f52d166f780c550d492e8827
Vitor Lessa Cavalcanti de Araujo <1192309500@acd.grupotiradentes.com>
2020-02-11 22:01:01 -0300

    exercicio 1

---
+++ src/Cliente.java
@@ -0,0 +1,19 @@
+
+public class Cliente {
+
+	private String cpf;
+	private String nome;
+	 
+	public String getcpf() {
+		return cpf;
+	}
+	public void setcpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getnome() {
+		return nome;
+	}
+	public void setnome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ src/teste.java
@@ -0,0 +1,11 @@
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cliente c1 = new 
+		
+	}
+
+}
