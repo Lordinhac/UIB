2107b293eb7af7b7b3fb55d101f73391be6a8163
Vítor Lessa <vitor@LAPTOP-B5OJ9H9E.(none)>
2020-02-13 08:48:18 -0300

    add Conta

---
+++ src/Conta.java
@@ -0,0 +1,20 @@
+
+public class Conta {
+
+	private String numero;
+	private String saldoDaConta;
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getSaldoDaConta() {
+		return saldoDaConta;
+	}
+	public void setSaldoDaConta(String saldoDaConta) {
+		this.saldoDaConta = saldoDaConta;
+	}
+	
+}
